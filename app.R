f9f1787e2b449ffeb6d0fefcbabffb9253c7132d
AlexanderHolmes0 <aholme27@vols.utk.edu>
2023-03-10 20:09:00 -0500

    Interp Update

--- app.R
+++ app.R
@@ -25,7 +25,7 @@ mytheme <- create_theme(
   )
 )
 
-interp <- tq_get("AA", get = "stock.prices" ,from="2000-01-01",complete_cases = TRUE) %>% # add something with date ranges to capture the trades
+interp <- tq_get("AA", get = "stock.prices" ,from="2000-01-01",to='2023-01-01',complete_cases = TRUE) %>% # add something with date ranges to capture the trades
   tq_transmute(mutate_fun = to.monthly, indexAt = "lastof") %>%
   mutate(
     date = yearmonth(date),
@@ -72,11 +72,11 @@ sidebar <- dashboardSidebar(
     ),
     menuItem("User Guide",
       icon = icon("user"), tabName = "user",
-      badgeLabel = "README", badgeColor = "green"
+      badgeLabel = "README", badgeColor = "light-blue"
     ),
-    menuItem("Example Interpretations",
+    menuItem("Ex. Interpretations",
              icon = icon("pen"), tabName = "interpret",
-             badgeLabel = "In Dev", badgeColor = "orange"
+             badgeLabel = "NEW", badgeColor = "green"
     ),
     menuItem("Github",
       icon = icon("github"),
@@ -106,14 +106,14 @@ sidebar <- dashboardSidebar(
     selectizeInput("model",
       "Model Type",
       selected = "X11",
-      choices = c("X11", "SEATS", "STL", "Classic - Multi", "Classic - Add")
+      choices = c("X11", "STL", "Classic - Multi", "Classic - Add")#add seats back later once fixed
     ),
     img(src = "giphy.gif", align = "center", height = "230px", width = "230px")
   )
 )
 
 body <- dashboardBody(
-  autoWaiter(id=c("series","season","ggseason","autoq","decompq",
+  autoWaiter(color = "#d81b60" ,id=c("series","season","ggseason","autoq","autolag","decompq",
                   "interp_series","interp_season","interp_ggseason",
                   "interp_autoq","interp_decompq"),
              fadeout = TRUE), 
@@ -143,8 +143,9 @@ body <- dashboardBody(
     tabItem(
       tabName = "auto",
       h2("Autocorrelation"),
-      box(title = textOutput("autoo"), background = "maroon", plotlyOutput("autoq"), width = 8),
+      box(title = textOutput("autoo"), background = "maroon",plotlyOutput("autoq"),hr(), plotlyOutput("autolag") ,width = 8),
       setShadow(id = "autoq"),
+      setShadow(id = "autolag"),
       box(title = "Maximum Lag", sliderInput("lags", "Lag Max Value", value = 12, min = 1, max = 100,
                                              animate = animationOptions(interval = 1500, loop = TRUE)), width = 4)
     ),
@@ -168,11 +169,11 @@ body <- dashboardBody(
       tabName = "interpret",
       h2(id='tittle',"EXAMPLE: Interpretation of Plots (Sry Plots Locked)"),
       animateUI('tittle',animation = 'float'),
-      fluidRow(box(id='box1',title = "Series: Alcoa Corporation Common Stock" ,plotlyOutput("interp_series"),verbatimTextOutput('words_series'), background = "maroon"),
-               box(id='box2',title = "Seasonality: Alcoa Corporation Common Stock" ,plotlyOutput("interp_season"),verbatimTextOutput('words_seasonal'),background = "maroon")),
-      fluidRow(box(id='box3',title = "Seasonaltiy: Alcoa Corporation Common Stock" ,plotlyOutput("interp_ggseason"),verbatimTextOutput('words_ggseason'),background = "maroon"),
-               box(id='box4',title = "Autocorrelation: Alcoa Corporation Common Stock" ,plotlyOutput("interp_autoq"),verbatimTextOutput('words_autoq'),background = "maroon"),
-               box(id='box5',title = "Decomposition: Alcoa Corporation Common Stock" ,plotlyOutput("interp_decompq"),verbatimTextOutput('words_decompq'),background = "maroon")),
+      fluidRow(box(id='box1',title = "Series: Alcoa Corporation Common Stock" ,plotlyOutput("interp_series"),hr(), box(title = "Interpretation",textOutput('words_series'),background = "light-blue", width=12), background = "maroon"),
+               box(id='box2',title = "Seasonality: Alcoa Corporation Common Stock" ,plotlyOutput("interp_season"),hr(), box(title = "Interpretation",textOutput('words_seasonal'),background = "light-blue", width=12),background = "maroon")),
+      fluidRow(box(id='box3',title = "Seasonaltiy: Alcoa Corporation Common Stock" ,plotlyOutput("interp_ggseason"),hr(), box(title = "Interpretation",textOutput('words_ggseason'),background = "light-blue", width=12),background = "maroon"),
+               box(id='box4',title = "Autocorrelation: Alcoa Corporation Common Stock" ,plotlyOutput("interp_autoq"),hr(), box(title = "Interpretation",textOutput('words_autoq'),background = "light-blue", width=12),background = "maroon"),
+               box(id='box5',title = "Decomposition: Alcoa Corporation Common Stock" ,plotlyOutput("interp_decompq"),hr(), box(title = "Interpretation",textOutput('words_decompq'),background = "light-blue", width=12),background = "maroon")),
       setShadow(id ="box1"),
       setShadow(id ="box2"),
       setShadow(id ="box3"),
@@ -225,10 +226,12 @@ server <- function(input, output, session) {
     
   })
   output$words_series <- renderText({
-    paste("Interpretation: This is a non-monotonic trend series that has great direction changes.",
-          "Seasonal variance is also non-monotonic with drastic changes throughout the years.",
+    paste("This series exhibits some irregular trend and seasonal behaviors.",
+          "It also exhibits a non-monotonic seasonal variance increase with drastic changes throughout the years.",
           "Pretty huge drop during the 2008-2009 financial crisis.",
-          sep="\n")
+          "In recent history, there has been a huge increase in Aloca stock price due to the Russia-Ukraine war.",
+          "The different colors of vertical lines indicate where a senator bought or sold Alcoa stock."
+          )
   })
   output$interp_decompq <- renderPlotly({
     interp %>%
@@ -238,11 +241,12 @@ server <- function(input, output, session) {
     
   })
   output$words_decompq <- renderText({
-    paste("This plot displays all the above in one format for easy viewing.",
+    paste("This plot displays all the above in one plot for easy viewing.",
           "Contains one additional interesting plot (remainder).",
           "May 2008 and March 2022 both show up as pretty big outliers",
           "the selected model can not account for with trend and seasonal components.",
-          sep="\n")
+          "Trend is also seen for the first time here which is just a moving average of the series.",
+          "Alcoa stock had a pretty wavy trend with some major drops aligning with the highlighted dates above.")
   })
   output$interp_autoq <- renderPlotly({
         interp %>%
@@ -253,7 +257,9 @@ server <- function(input, output, session) {
     paste("Since trend is pretty important to this series, we see a downward slide.",
           "We see a reversal in the slide to show more and more positive correlation values.",
           "This makes sense because the values are somewhat closer together when lagged beyond 24 months.",
-          sep="\n")
+          "The graphic depicts the correlation of the series with the lagged series for certain months.",
+          "*'ACF' is the correlation factor with the lagged series.*"
+          )
   })
   output$interp_season <- renderPlotly({
     interp %>%
@@ -263,9 +269,9 @@ server <- function(input, output, session) {
   })
   output$words_seasonal <- renderText({
     paste("Due to how crazy this plot is, there is a consistent drop in seasonal variance.",
-          "Seasonal variance was at all time lows during the 2014-2015 era.",
-          "Seasonality began to start increasing after that period.",
-          sep="\n")
+          "Seasonal variance during September was at all time lows during the 2014-2015 era.",
+          "Seasonality began increasing after that period.",
+          "There's a cone like shape for the seasonal variance in this plot.")
   })
   output$interp_ggseason <- renderPlotly({
     interp %>%
@@ -276,8 +282,8 @@ server <- function(input, output, session) {
   output$words_ggseason <- renderText({
     paste("This plot identifies the massive changes in seasonal variance by the year.",
           "The cyclical nature of the aluminum industry is exhibited well here.",
-          "September has turned from a mostly negative month to a positive month.",
-          sep="\n")
+          "September in recent years has become less of a month of decline for Alcoa common stock.",
+          "Seasonality overall has become less variant in nature and stays relatively close to the trend.")
   })
   
   
@@ -292,11 +298,15 @@ server <- function(input, output, session) {
         "dates",
         start = min(senate$Transaction.Date) - 6,
       )
-    
+    if (any(input$type %in% senate$Type)){
       senate <- senate %>%
         filter(Type %in% input$type)
+     senate 
+    }else {
+      shinyCatch(message("No Transaction Types like that YET!"), position = "top-right")
+      NULL
+    }
       
-      senate
     } else {
       shinyCatch(message("No Stonk Trading Senators YET!"), position = "top-right")
       NULL
@@ -420,10 +430,27 @@ server <- function(input, output, session) {
         ACF(!!sym(input$column), lag_max = input$lags) %>%
         autoplot()
     } else {
-      shinyCatch(position = "top-right", message("Not enuff time mannn!"))
+      shinyCatch(position = "top-right", message("Not enuff time!"))
     }}
   })
-
+  
+  output$autolag <- renderPlotly({
+    if(!is.null(models())){
+      if (nrow(stock()) > 1) {
+        lagged <- stock() %>% 
+          select(date,!!sym(input$column))
+        
+        
+        lagged %>%
+          autoplot()+
+          geom_line(aes(x=date,y=lag(!!sym(input$column),input$lags)),color="#0066B6")+
+          ggtitle("Lagged Series")
+        
+      } else {
+        shinyCatch(position = "top-right", message("Not enuff time!"))
+      }}
+  })
+  
   output$decompq <- renderPlotly({
     if( !is.null(models())){
       autoplot(models())
@@ -435,20 +462,4 @@ server <- function(input, output, session) {
 # Run the application
 shinyApp(ui = ui, server = server)
 
-# library(tidyquant)
-# #install.packages("tidyquant")
-# library(tidyverse)
-# library(fpp3)
-# stonk <- tq_get("AA", get = "stock.prices" ,complete_cases = TRUE) %>% # add something with date ranges to capture the trades
-#   tq_transmute(mutate_fun = to.monthly, indexAt = "lastof") %>%
-#   mutate(
-#     date = yearmonth(date),
-#     across(2:7, \(x) round(x, 3))
-#   ) %>%
-#   as_tsibble(index = date)
-# 
-# try <- stonk %>%
-#   model(X_13ARIMA_SEATS(adjusted ~ seats())) %>%
-#   components()
-# print(try)
-# autoplot(try)
+
